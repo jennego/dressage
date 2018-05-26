df0e94ab5424dbc980cba5dcb2f0d8277f25582f
Jen Chow <jennego@gmail.com>
2018-05-25 20:47:58 -0700

    Install devise and simple form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :dressage_tests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/about', to: 'welcome#about'
