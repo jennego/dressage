32ebce65ba7142f9ad26c3acb1c9c0bbb80758df
Jen Chow <jennego@gmail.com>
2018-06-21 21:36:04 -0700

    Add link to search result.

--- app/models/dressage_test.rb
+++ app/models/dressage_test.rb
@@ -2,7 +2,7 @@ class DressageTest < ApplicationRecord
   has_many :moves, dependent: :destroy
   accepts_nested_attributes_for :moves
   include PgSearch
-  pg_search_scope :search_by_test, against: [:name, :org_name, :level, :year],
+  pg_search_scope :search_by_test, against: [:org_name, :year, :level, :name],
   using: {
     tsearch: {
       prefix: true,
