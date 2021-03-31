dfe8c94929de0a19ccde4796d169e0d392fcf470
Jennifer Chow <jennego@gmail.com>
2021-03-30 19:48:29 -0700

    move index to another page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 resources :dressage_tests 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/about', to: 'welcome#about'
- root 'dressage_tests#index'
+ root 'welcome#index'
  devise_for :users
 
 
