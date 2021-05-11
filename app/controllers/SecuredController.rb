19ffa6427201351d2a27c66d09c8ffc36e1ed31c
Jennifer Chow <jennego@gmail.com>
2021-05-11 00:06:02 -0700

    Readme update

--- app/controllers/SecuredController.rb
+++ app/controllers/SecuredController.rb
@@ -10,7 +10,7 @@ class SecuredController < ApplicationController
     def admin_logged_in_using_omniauth?
     redirect_back(fallback_location: '/') unless admin_user_logged_in?
     if admin_user_logged_in? == false
-    flash.alert = "You are not authorized to do that. You need to be logged in as an Admin,."
+    flash.alert = "You are not authorized to do that. You need to be logged in as an Admin."
     end
   end
 end
