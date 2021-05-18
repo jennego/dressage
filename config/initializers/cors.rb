7651c90557fec58012d02b42b91b8738b30fbb79
Jennifer Chow <jennego@gmail.com>
2021-05-18 15:53:19 -0700

    why did heroku crash?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true do
   allow do
-    origins 'localhost:4000', 'https://dressage-tests.herokuapp.com', 'https://dressagetests.netlify.app', 'https://devdressage.netlify.app'
+    origins 'localhost:4000','https://dressage-tests.herokuapp.com','https://dressagetests.netlify.app','https://devdressage.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
